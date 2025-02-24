4a5645c19b020c2fe7165307e4e13769a10d4c51
denis <denis.yarekha@mail.ru>
2025-02-24 20:52:08 +0300

    com12

--- appCodeAuth.py
+++ appCodeAuth.py
@@ -41,5 +41,5 @@ def generate_code():
     session.clear()
     return f"code: {code}"
 if __name__ == "__main__":
-    port = int(os.getenv('PORT',5000))
-    app.run(host='0.0.0.0',port=port)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
